230a1595b9d1cb3a07e24ef95342067aea326016
beganov1c <tbeganovic1@etf.unsa.ba>
2022-01-22 19:12:43 +0100

    Dodan about prozor.

---
+++ src/ba/unsa/etf/rpr/AboutController.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rpr;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class AboutController {
+    public Button cancelBtn;
+
+    public void cancelAction(ActionEvent actionEvent) {
+        Stage stage = (Stage) cancelBtn.getScene().getWindow();
+        stage.close();
+    }
+}
