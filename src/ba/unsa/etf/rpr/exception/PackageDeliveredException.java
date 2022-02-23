6ec742aa0b0b1c675681ddc62d1092682dfff948
beganov1c <tbeganovic1@etf.unsa.ba>
2022-02-23 03:33:47 +0100

    Dodani vlastiti izuzeci.

---
+++ src/ba/unsa/etf/rpr/exception/PackageDeliveredException.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr.exception;
+
+public class PackageDeliveredException extends RuntimeException {
+
+    public PackageDeliveredException(String message) {
+        super(message);
+    }
+}
---
+++ src/ba/unsa/etf/rpr/exception/PackageErrorException.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr.exception;
+
+public class PackageErrorException extends RuntimeException {
+
+    public PackageErrorException(String message) {
+        super(message);
+    }
+}
+
