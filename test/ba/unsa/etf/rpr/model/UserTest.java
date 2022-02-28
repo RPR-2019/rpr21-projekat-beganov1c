40b554ceb54e733074ae6ebb27b53552ce1b2a9d
beganov1c <tbeganovic1@etf.unsa.ba>
2022-02-28 03:14:18 +0100

    UserTest added

---
+++ test/ba/unsa/etf/rpr/model/UserTest.java
@@ -0,0 +1,59 @@
+package ba.unsa.etf.rpr.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    private User user = new User(1,"Name","test","test","test",77220);
+
+    @Test
+    void getAddress() {
+        assertEquals("test",user.getAddress());
+    }
+
+    @Test
+    void setAddress() {
+        user.setAddress("new");
+        assertEquals("new",user.getAddress());
+    }
+
+    @Test
+    void getId() {
+        assertEquals(1,user.getId());
+    }
+
+    @Test
+    void setId() {
+        user.setId(-1);
+        assertEquals(-1,user.getId());
+    }
+
+    @Test
+    void getCity() {
+        assertEquals("test",user.getCity());
+    }
+
+    @Test
+    void setCity() {
+        user.setCity("city");
+        assertEquals("city",user.getCity());
+    }
+
+    @Test
+    void getZipCode() {
+        assertEquals(77220,user.getZipCode());
+    }
+
+    @Test
+    void setZipCode() {
+        user.setZipCode(123);
+        assertEquals(123,user.getZipCode());
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("Name",user.toString());
+    }
+}
