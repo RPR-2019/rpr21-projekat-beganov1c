68679a3d145d1268e42010bbe2856d45c6a67d5a
beganov1c <tbeganovic1@etf.unsa.ba>
2022-02-28 02:24:31 +0100

    About window fixed and added translation

--- src/ba/unsa/etf/rpr/controller/AboutController.java
+++ src/ba/unsa/etf/rpr/controller/AboutController.java
@@ -1,11 +1,28 @@
 package ba.unsa.etf.rpr.controller;
 
+import javafx.application.HostServices;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.stage.Stage;
 
 public class AboutController {
+    static HostServices hostServices ;
     public Button cancelBtn;
+
+    public static void setGetHostController(HostServices hostServices)
+    {
+        AboutController.hostServices = hostServices;
+    }
+
+    @FXML
+    Hyperlink link=new Hyperlink();
+
+    @FXML
+    public void initialize(){
+        link.setOnAction(e->hostServices.showDocument("https://github.com/beganov1c"));
+    }
 
     public void cancelAction(ActionEvent actionEvent) {
         Stage stage = (Stage) cancelBtn.getScene().getWindow();
