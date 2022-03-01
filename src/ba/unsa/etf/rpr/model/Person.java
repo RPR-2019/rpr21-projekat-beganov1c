5b18fcfee16818ddaa0caa633a158ae080d16b0f
beganov1c <92276578+beganov1c@users.noreply.github.com>
2022-03-01 23:18:57 +0100

    Add files via upload

---
+++ src/ba/unsa/etf/rpr/model/Person.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rpr.model;
+
+public abstract class Person {
+
+    private String name;
+    private String telephoneNumber;
+
+    protected Person(String name, String telephoneNumber) {
+        this.name = name;
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+}
